c7aa35953506067b00448cd00b17e4f90eaa4bb9
Pernilla Sterner <psterner@hotmail.com>
2024-02-18 20:51:05 +0100

    Put data about pastery in seperate file

---
+++ code/data.js
@@ -0,0 +1,41 @@
+// Data
+const typeOfPastery = [
+  {
+    id: "regular",
+    label: "Regular",
+    value: "regular",
+    name: "type_of_pastery",
+  },
+  {
+    id: "gluten",
+    label: "Gluten free",
+    value: "gluten",
+    name: "type_of_pastery",
+  },
+  {
+    id: "lactose",
+    label: "Lactose free",
+    value: "lactose",
+    name: "type_of_pastery",
+  },
+];
+
+const subtypeOfPastery = [
+  {
+    id: "blueberry",
+    label: "Blueberry",
+    class: "choice-btn",
+  },
+  {
+    id: "vanilla",
+    label: "Vanilla",
+    class: "choice-btn",
+  },
+  {
+    id: "chocolate",
+    label: "Chocolate",
+    class: "choice-btn",
+  },
+];
+
+export { typeOfPastery, subtypeOfPastery };
