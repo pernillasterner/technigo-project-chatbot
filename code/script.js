858606ce990f5328a48f16d50561a9789e359b40
Pernilla Sterner <psterner@hotmail.com>
2024-02-18 14:36:26 +0100

    Clean up

--- code/script.js
+++ code/script.js
@@ -1,18 +1,12 @@
-// DOM selectors (variables that point to selected DOM elements) goes here 👇
 const chat = document.getElementById("chat");
 const nameForm = document.getElementById("name-form");
+const nameInput = document.getElementById("name-input");
 const start = document.getElementById("start");
-
-// Hiding elements before start
-chat.style.display = "none";
-nameForm.style.display = "none";
 
 // Functions goes here 👇
 
 // A function that will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // The if statement checks if the sender is the user and if that's the case it inserts
-  // an HTML section inside the chat with the posted message from the user
   if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
@@ -22,8 +16,6 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `;
-    // The else if statement checks if the sender is the bot and if that's the case it inserts
-    // an HTML section inside the chat with the posted message from the bot
   } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
@@ -42,27 +34,28 @@ const showMessage = (message, sender) => {
 
 // A function to start the conversation
 const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
   showMessage("Hello there, what's your name?", "bot");
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
 };
 
-// Eventlisteners goes here 👇
+const handleNameInput = (event) => {
+  event.preventDefault();
 
-// Here we invoke the first function to get the chatbot to ask the first question when
-// the website is loaded. Normally we invoke functions like this: greeting()
-// To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
-// and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds
-// This means the greeting function will be called one second after the website is loaded.
+  const name = nameInput.value;
+  showMessage(name, "user");
+  console.log(name);
+};
 
+// A function to display form when button gets clicked on
 const startIcecreamJourney = () => {
   start.style.display = "none";
   chat.style.display = "flex";
   nameForm.style.display = "flex";
   setTimeout(greetUser, 1000);
 };
+
+// Eventlisteners goes here 👇
+
+setTimeout(greetUser, 1000);
 
 // Add eventlistener to start button
 start.addEventListener("click", startIcecreamJourney);
